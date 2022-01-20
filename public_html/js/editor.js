6f22cfcfbce84ef70f9a7be8a222a54c6ee556e6
Brian Sandall <brian.sandall@nescorentals.com>
2022-01-19 18:31:26 -0800

    Functional data editor

---
+++ public_html/js/editor.js
@@ -0,0 +1,368 @@
+/**
+ * Copies the specified element to the target container
+ * @param entry_id String, id of element to clone
+ * @param target_id String, target container id
+ */
+function addEntry(entry_id, target_id) {
+	let entry = document.getElementById(entry_id);
+	let target = document.getElementById(target_id);
+	return addEntryToElement(entry, target);
+}
+
+/**
+ * Copies the specified element to the target container
+ * @param entry  HTML DOM element of entry to be added
+ * @param target HTML DOM element of container
+ */
+function addEntryToElement(entry, target) {
+	if (!entry || !target) { return false; }
+	if (!validateInputGroup(entry, true)) {
+		document.forms[0].reportValidity();
+		return false;
+	}
+	let clone = entry.cloneNode(true);
+	// Clone doesn't handle selected options on <select> elements, so do it manually
+	let a = entry.getElementsByTagName('SELECT');
+	if (a.length > 0) {
+		let b = clone.getElementsByTagName('SELECT');
+		for (let i = 0; i < a.length; ++i) {
+			for (let j = 0; j < a[i].options.length; ++j) {
+				if (a[i].options[j].selected) {
+					b[i].options[j].selected = true;
+				}
+			}
+		}
+	}
+	clone.removeAttribute('id');
+	Array.from(clone.children).forEach(child => {
+		child.removeAttribute('id');
+		child.children[0].classList.remove('no_validate');
+	});
+	// Assumes a specific HTML structure for the Add/Remove link's parent and container
+	let remove = getRemoveEntryElement();
+	clone.children[0].replaceChild(remove, clone.children[0].children[0]);
+	target.appendChild(clone);
+	// Reset form inputs on original element
+	resetFormInputs(entry);
+	return true;
+}
+
+function getRemoveEntryElement() {
+	let a = document.createElement('A');
+	a.href = 'javascript:;';
+	a.title = 'Delete this entry';
+	a.innerHTML = '[&minus;]';
+	a.addEventListener('click', removeEntry);
+	return a;
+}
+
+function removeEntry() {
+	let entry = this.parentNode.parentNode;
+	entry.remove();
+}
+
+/**
+ * Refreshes the Encounter Entry input templates for Categories and Filters using current form data.
+ */
+function refreshDataElements() {
+	buildCategoryOptions('select_entry_category');
+	buildFilterElements('entries_filters_container');
+}
+
+/**
+ * Updates new Encounter Entry <select> element options for Weight Category
+ * @param target_id String, target container id
+ */
+function buildCategoryOptions(target_id) {
+	let target = document.getElementById(target_id);
+	if (!target) {
+		return;
+	}
+	target.replaceChildren();
+	let default_category = document.createElement('OPTION');
+	default_category.innerHTML = 'None';
+	target.appendChild(default_category);
+	let categories = getCategories();
+	for (let i = 0; i < categories.length; ++i) {
+		let cat = document.createElement('OPTION');
+		cat.value = i;
+		cat.innerHTML = categories[i].name;
+		target.appendChild(cat);
+	}
+}
+
+/**
+ * Builds the Filter <select> element templates used for new Encounter Entries
+ * and places them in the target container, replacing any existing contents.
+ * @param target_id String, target container id
+ */
+function buildFilterElements(target_id) {
+	let filters = getFilters();
+	let target = document.getElementById(target_id);
+	if (!target || !filters || filters.size < 1) {
+		return;
+	}
+	target.replaceChildren();
+	Array.from(filters.values()).forEach(v => {
+		try {
+			let obj = new Filter(v.name, v.values);
+			let el = obj.getHtmlElement(null, 'filter_' + obj.name + '[]');
+			if (el) {
+				target.appendChild(el);
+			}
+		} catch (err) {
+			console.log("Ignored invalid filter: " + err);
+		}
+	});
+	return false;
+}
+
+/**
+ * @return Array<Category> based on user inputs
+ */
+function getCategories() {
+	let names = document.getElementsByName('categories_name[]');
+	let weights = document.getElementsByName('categories_weight[]');
+	if (names.length !== weights.length) {
+		throw new Error('Invalid input data for weight categories');
+	}
+	let categories = [];
+	for (let i = 0; i < names.length; ++i) {
+		if (names[i].classList.contains('no_validate')) {
+			continue; // do not process templates until added
+		}
+		try {
+			let weight = (weights[i].value === undefined ? 1 : parseInt(weights[i].value));
+			let obj = new Category(names[i].value, weight);
+			categories.push(obj);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	return categories;
+}
+
+/**
+ * @return Map<String, Filter> based on user inputs, keyed by the filter's name
+ */
+function getFilters() {
+	let names = document.getElementsByName('filters_name[]');
+	let values = document.getElementsByName('filters_values[]');
+	if (names.length !== values.length) {
+		throw new Error('Invalid input data for filters');
+	}
+	let filters = new Map();
+	for (let i = 0; i < names.length; ++i) {
+		if (names[i].classList.contains('no_validate')) {
+			continue; // do not process templates until added
+		}
+		try {
+			let entries = values[i].value.split("\n");
+			let obj = new Filter(names[i].value, entries);
+			filters.set(obj.name, obj);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	return filters;
+}
+
+/**
+ * @param filters Map<String, Filter> of existing filters
+ * @return Array<Entry> based on user inputs
+ */
+function getEntries(filters) {
+	let names = document.getElementsByName('entries_name[]');
+	let weights = document.getElementsByName('entries_weight[]');
+	let weight_ids = document.getElementsByName('entries_category[]');
+	let sources = document.getElementsByName('entries_ref_src[]');
+	let pages = document.getElementsByName('entries_ref_page[]');
+	if (names.length !== weights.length || weights.length !== weight_ids.length || weight_ids.length !== sources.length || sources.length !== pages.length) {
+		throw new Error('Invalid input data for encounter entries');
+	}
+	let entries_filters = new Map();
+	if (filters instanceof Map) {
+		Array.from(filters.keys()).forEach(k => {
+			let name = 'filter_' + k + '[]';
+			let inputs = document.getElementsByName(name);
+			if (!inputs || inputs.length !== names.length) {
+				throw new Error('Invalid input data for encounter entries filter: ' + k);
+			}
+			entries_filters.set(k, inputs);
+		});
+	}
+	let entries = [];
+	try {
+		for (let i = 0; i < names.length; ++i) {
+			if (names[i].classList.contains('no_validate')) {
+				continue; // do not process templates until added
+			}
+			try {
+				let weight = parseInt(weights[i].value);
+				if (weight === NaN) {
+					weight = undefined;
+				}
+				let weight_id = parseInt(weight_ids[i].value);
+				if (weight_id === NaN) {
+					weight_id = undefined;
+				}
+				let ref_src = sources[i].value;
+				let ref_page = pages[i].value;
+				let entry_filters = new Map();
+				entries_filters.forEach(function(values, key) {
+					let selected = [];
+					Array.from(values[i].selectedOptions).forEach(v => {
+						let parsed = parseInt(v.value);
+						let val = (parsed === NaN ? v.value : parsed);
+						selected.push(val);
+					});
+					if (selected.length > 0) {
+						entry_filters.set(key, selected);
+					}
+				});
+				let obj = new Entry(names[i].value, weight, weight_id, ref_src, ref_page, entry_filters);
+				entries.push(obj);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	} catch (err) {
+		console.log(err);
+	}
+	return entries;
+}
+
+function exportData() {
+	let _form = document.getElementById('data_form');
+	if (!validateForm(_form)) {
+		_form.reportValidity();
+		return;
+	}
+	let categories = getCategories();
+	let filters = getFilters();
+	let entries = getEntries(filters);
+	let data = new Object();
+	if (categories.length > 0) {
+		data.categories = categories;
+	}
+	if (filters.size > 0) {
+		data.filters = Array.from(filters.values());
+	}
+	if (entries.length > 0) {
+		data.entries = entries;
+	}
+	// Paste data into export box
+	let data_box = document.getElementById('data_box');
+	if (data_box) {
+		data_box.value = JSON.stringify(data, replacer);
+	}
+}
+
+function importData() {
+	let data_box = document.getElementById('data_box');
+	if (!data_box || data_box.value.length < 1) {
+		alert('Please enter data in the box below before importing');
+		return;
+	}
+	let data = [];
+	try {
+		data = JSON.parse(data_box.value, reviver);
+	} catch (err) {
+		alert('Import failed: invalid JSON format');
+		return;
+	}
+	// Import categories
+	if (data.categories) {
+		let table = document.getElementById('categories');
+		let template = document.getElementById('categories_new');
+		table.replaceChildren();
+		table.appendChild(template);
+		let i = 0;
+		data.categories.forEach(v => {
+			try {
+				let obj = new Category(v.name, v.weight);
+				template.children[1].children[0].value = obj.name;
+				template.children[2].children[0].value = obj.weight;
+				addEntryToElement(template, table);
+			} catch (err) {
+				console.log("Ignored invalid filter: " + err);
+			}
+		});
+		buildCategoryOptions('select_entry_category');
+	}
+	// Import filters
+	if (data.filters) {
+		let table = document.getElementById('filters');
+		let template = document.getElementById('filters_new');
+		table.replaceChildren();
+		table.appendChild(template);
+		data.filters.forEach(v => {
+			try {
+				let obj = new Filter(v.name, v.values);
+				template.children[1].children[0].value = obj.name;
+				template.children[2].children[0].value = obj.values.join("\n");
+				addEntryToElement(template, table);
+			} catch (err) {
+				console.log("Ignored invalid filter: " + err);
+			}
+		});
+		buildFilterElements('entries_filters_container');
+	}
+	// Import encounter entries
+	if (data.entries) {
+		let table = document.getElementById('entries');
+		let template = document.getElementById('entries_new');
+		let select_weight_category = document.getElementById('select_entry_category');
+		table.replaceChildren();
+		table.appendChild(template);
+		data.entries.forEach(v => {
+			try {
+				let obj = new Entry(v.name, v.weight, v.weight_id, v.ref_src, v.ref_page, v.filters);
+				template.children[1].children[0].value = obj.name;
+				template.children[2].children[0].value = obj.weight;
+				if (select_weight_category) {
+					for (let i = 0; i < select_weight_category.options.length; ++i) {
+						// Non-strict comparison
+						if (v.weight_id == select_weight_category.options[i].value) {
+							select_weight_category.selectedIndex = i;
+							break;
+						}
+					}
+				}
+				template.children[3].children[0].value = obj.ref_src;
+				template.children[4].children[0].value = obj.ref_page;
+				if (obj.filters.size > 0) {
+					let selects = template.children[5].getElementsByTagName('SELECT');
+					for (let i = 0; i < selects.length; ++i) {
+						let n = 'filter_'.length;
+						let filter_key = selects[i].name.substr(n, selects[i].name.length - (n + 2));
+						let filter_values = obj.filters.get(filter_key);
+						if (filter_values) {
+							filter_values.forEach(val => {
+								selects[i].options[val].selected = true;
+							});
+						}
+					}
+				}
+				addEntryToElement(template, table);
+			} catch (err) {
+				console.log("Ignored invalid filter: " + err);
+			}
+		});
+	}
+	let _form = document.getElementById('data_form');
+	if (!validateForm(_form)) {
+		alert('One or more errors were detected - see form inputs');
+	}
+}
+
+window.addEventListener("load", function(event) {
+	document.getElementById('btn_validate').addEventListener('click', function(e) {
+		let f = document.getElementById('data_form');
+		if (validateForm(f)) {
+			alert('No input errors were detected');
+		} else {
+			alert('One or more errors were detected - see form inputs');
+		}
+	});
+});
---
+++ public_html/js/main.js
@@ -0,0 +1,293 @@
+/**
+ * Class that excludes properties with the default value from JSON output.
+ */
+class MinJson {
+	isDefault(key) {
+		return false;
+	}
+	toJSON() {
+		let json = {};
+		for (let k in this) {
+			if (!this.isDefault(k)) {
+				json[k] = this[k];
+			}
+		}
+		return json;
+	}
+}
+
+/**
+ * Object for encounter frequency categories
+ * @property name   String, unique name for the weight category, e.g. "Very Rare"
+ * @property weight Integer, weighted prevalence of this category (minimum = 1)
+ */
+class Category extends MinJson {
+	constructor(name, weight = 1) {
+		super();
+		if (typeof name !== 'string' || name.length < 1) {
+			throw new Error('Category.name must be a non-empty String');
+		}
+		if (!Number.isSafeInteger(weight) || weight < 1) {
+			throw new Error('Category.weight must be an integer greater than 0');
+		}
+		this.name = name;
+		this.weight = weight;
+	}
+	isDefault(key) {
+		switch (key) {
+		case 'weight': return this.weight === 1;
+		}
+		return false;
+	}
+}
+
+/**
+ * A filter defines a set of valid values, any number of which may be applied when generating an encounter table.
+ * @property name   String, unique name for the filter grouping, e.g. "Climate"
+ * @property values Array<String> containing all valid values for this filter
+ */
+class Filter extends MinJson {
+	constructor(name, values = []) {
+		super();
+		if (typeof name !== 'string' || name.length < 1) {
+			throw new Error('Filter.name must be a non-empty String');
+		}
+		this.name = name;
+		if (Array.isArray(values)) {
+			this.values = values.filter(s => typeof s === 'string' && s.length > 0);
+		} else {
+			this.values = [];
+		}
+	}
+	/**
+	 * @param id   String, desired element ID, if any
+	 * @param name String, desired element name; defaults to the filter name
+	 * @return HTML <select> element for the provided filter, or false if not valid
+	 */
+	getHtmlElement(id, name) {
+		if (this.values.length < 1) {
+			return false;
+		}
+		let div = document.createElement('DIV');
+		div.classList.add('fleft');
+		div.classList.add('container');
+		let lab = document.createElement('LABEL');
+		if (id) {
+			lab.htmlFor = id;
+		}
+		lab.innerHTML = this.name;
+		div.appendChild(lab);
+		div.appendChild(document.createElement('BR'));
+		let sel = document.createElement('SELECT');
+		if (id) {
+			sel.id = id;
+		}
+		const MAX_SELECT_SIZE = 5;
+		sel.name = (name ? name : this.name);
+		sel.setAttribute('multiple', true);
+		sel.setAttribute('size', Math.min(this.values.length, MAX_SELECT_SIZE));
+		for (let i = 0; i < this.values.length; ++i) {
+			let v = this.values[i];
+			let opt = document.createElement('OPTION');
+			opt.value = i;
+			opt.innerHTML = v;
+			sel.appendChild(opt);
+		};
+		div.appendChild(sel);
+		return div;
+	}
+}
+
+/**
+ *
+ * @property name      String, unique name for the entry, e.g. "Goblin"
+ * @property weight    Integer, individual encounter weight
+ * @property weight_id Integer, encounter weight category id
+ * @property ref_src   String, optional name of reference source, e.g. "HoB 1" or "www.monsters.com"
+ * @property ref_page  String, optional reference page number, e.g. "140" or "/goblin"
+ * @property filters   Map<String, Array<Integer>> of applicable Filter value array indices keyed by Filter name
+ */
+class Entry extends MinJson {
+	constructor(name, weight = 1, weight_id = -1, ref_src = '', ref_page = '', filters = null) {
+		super();
+		if (typeof name !== 'string' || name.length < 1) {
+			throw new Error('Entry.name must be a non-empty String');
+		}
+		if (!Number.isSafeInteger(weight) || weight < 1) {
+			throw new Error('Entry.weight must be an integer greater than 0');
+		}
+		if (!Number.isSafeInteger(weight_id)) {
+			throw new Error('Entry.weight_id must be an integer');
+		}
+		this.name = name;
+		this.weight = weight;
+		this.weight_id = weight_id;
+		this.ref_src = (typeof ref_src === 'string' ? ref_src : '');
+		this.ref_page = (typeof ref_page === 'string' ? ref_page : '');
+		if (filters instanceof Map) {
+			this.filters = filters;
+		} else {
+			this.filters = new Map();
+		}
+	}
+	isDefault(key) {
+		switch (key) {
+		case 'filters': return this.filters.size === 0;
+		case 'ref_src': return this.ref_src.length === 0;
+		case 'ref_page': return this.ref_page.length === 0;
+		case 'weight': return this.weight === 1;
+		case 'weight_id': return this.weight_id < 0;
+		}
+		return false;
+	}
+}
+
+/**
+ * JSON.stringify replacer function that handles Map objects
+ */
+function replacer(key, value) {
+	if (value instanceof Map) {
+		return {
+			dataType: 'Map',
+			value: Array.from(value.entries()),
+		};
+	}
+	return value;
+}
+
+/**
+ * JSON.stringify reviver function that handles Map objects
+ */
+function reviver(key, value) {
+	if (typeof value === 'object' && value !== null) {
+		if (value.dataType === 'Map') {
+			return new Map(value.value);
+		}
+	}
+	return value;
+}
+
+/**
+ * Resets child <input>, <select>, and <textarea> elements
+ * @param entry Parent element
+ */
+function resetFormInputs(entry) {
+	let inputs = entry.getElementsByTagName('INPUT');
+	for (let i = 0; i < inputs.length; ++i) {
+		let def = inputs[i].getAttribute('data-default');
+		inputs[i].value = def;
+	}
+	let selects = entry.getElementsByTagName('SELECT');
+	for (let i = 0; i < selects.length; ++i) {
+		let def = selects[i].getAttribute('data-default');
+		selects[i].selectedIndex = (def === null ? -1 : def);
+	}
+	let texts = entry.getElementsByTagName('TEXTAREA');
+	for (let i = 0; i < texts.length; ++i) {
+		let def = texts[i].getAttribute('data-default');
+		texts[i].value = def;
+	}
+}
+
+/**
+ * Checks validity of all child <input>, <select>, and <textarea> elements
+ * @param entry Parent element
+ * @param force Boolean, true to validate elements with the 'no_validate' tag
+ * @return false if validateInput() returns false for any of input element
+ */
+function validateInputGroup(entry, force = false) {
+	let is_valid = true;
+	let inputs = entry.getElementsByTagName('INPUT');
+	Array.from(inputs).forEach(e => {
+		if (!validateInput(e, force)) {
+			is_valid = false;
+		}
+	});
+	let selects = entry.getElementsByTagName('SELECT');
+	Array.from(selects).forEach(e => {
+		if (!validateInput(e, force)) {
+			is_valid = false;
+		}
+	});
+	let texts = entry.getElementsByTagName('TEXTAREA');
+	Array.from(texts).forEach(e => {
+		if (!validateInput(e, force)) {
+			is_valid = false;
+		}
+	});
+	return is_valid;
+}
+
+/**
+ * @param entry Form element to validate
+ * @param force Boolean, true to validate elements with the 'no_validate' tag
+ */
+function validateForm(entry, force = false) {
+	let is_valid = true;
+	Array.from(entry.elements).forEach(e => {
+		if (!validateInput(e, force)) {
+			is_valid = false;
+		}
+	});
+	return is_valid;
+}
+
+/**
+ * Checks the validity of a single <input>, <select>, or <textarea> element, updating the class list accordingly
+ * @param input HTML element to validate
+ * @param force Boolean, true to validate elements with the 'no_validate' tag
+ * @return true if valid or no validation required
+ */
+function validateInput(input, force = false) {
+	if (!force && input.classList.contains('no_validate')) {
+		input.classList.remove('error');
+		return true;
+	} else if (input.checkValidity()) {
+		input.classList.remove('error');
+		return true;
+	} else {
+		input.classList.add('error');
+		return false;
+	}
+}
+
+/**
+ * Displays the content associated with the selected tab and hides content associated with any other tabs in the group.
+ * Assumptions:
+ *  - a single parent element contains all tabs in any given group
+ *  - each tab is a clickable element (e.g. <a>) inside a parent element (e.g. <div> or <li>)
+ * @param container_id String, the id of the content container to be displayed for the tab; URL hash will be set to this value
+ */
+function selectTab(container_id) {
+	const prefix = 'tab-';
+	let tab = document.getElementById(prefix + container_id);
+	let container = document.getElementById(container_id);
+	if (container && tab) {
+		Array.from(tab.parentNode.children).forEach(function(e) {
+			e.classList.remove("tab_selected");
+			let el = document.getElementById(e.id.substr(prefix.length));
+			if (el) {
+				el.classList.add('hide');
+			}
+		});
+		container.classList.remove('hide');
+		tab.classList.add("tab_selected");
+		history.replaceState(undefined, undefined, '#'+container_id);
+	}
+	return false;
+}
+
+window.addEventListener("load", function(event) {
+	document.getElementById('copy_data_box').addEventListener('click', function(e) {
+		let target = document.getElementById(this.htmlFor);
+		navigator.clipboard.writeText(target.value).then(function() {
+			console.log('Copying to clipboard was successful.');
+		}, function(err) {
+			console.error('Failed to copy text: ', err);
+		});
+	});
+	if (document.location.hash) {
+		let hash = document.location.hash.substr(1, document.location.hash.length);
+		selectTab(hash);
+	}
+});
